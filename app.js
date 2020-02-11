e9f5825a86e6442653795ab2650a5123bbd2c1dd
Sedrick Keh <sedrickkeh@gmail.com>
2020-02-11 19:24:26 +0800

    set up database

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// set up mongoose connection
+var mongoose = require('mongoose');
+var db = require('./configs/database');
+mongoose.connect(db.config.uri, db.config.options);
+mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
---
+++ configs/database.js
@@ -0,0 +1,8 @@
+module.exports.config = {
+    uri: "mongodb+srv://arete:d%40mK%25M4e5lU3i@cluster0-xk87r.azure.mongodb.net/test?retryWrites=true&w=majority",
+    options: {
+        dbName: 'arete',
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    },
+}
