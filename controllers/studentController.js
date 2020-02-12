b476407492cbba00d61d6aaea8e691e717d65a11
Sedrick Keh <sedrickkeh@gmail.com>
2020-02-12 12:37:57 +0800

    implement basic create, delete, update functions

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -3,35 +3,88 @@ var Student = require('../models/student');
 var async = require('async');
 
 
-exports.student_list = function(req, res) {
-    res.send('NOT IMPLEMENTED');
+exports.student_list = function(req, res, next) {
+    Student.find()
+        .exec(function(err, list_students) {
+            if (err) {return next(err);}
+            res.json(list_students);
+        });
 };
 
 exports.student_detail = function(req, res, next) {
-    res.send('NOT IMPLEMENTED');
+    Student.findById(req.params.id)
+        .exec(function(err, student) {
+            if (err) { return next(err); }
+            if (student == null) {
+                var err = new Error('Student not found');
+                err.status = 404;
+                return next(err);
+            } 
+            res.json(student);
+        });
 };
 
 exports.student_create_get = function(req, res, next) {
-    res.send('NOT IMPLEMENTED');
+    res.send('Create GET not needed at this point');
 };
 
 exports.student_create_post = function(req, res, next) {
-    res.send('NOT IMPLEMENTED');
+    var student = new Student(req.body);
+    student.save()
+        .then(student => {
+            res.status(200).json({'status': 'created successfully', 'student': student});
+        })
+        .catch(err => {
+            res.status(400).send('creating failed');
+        });
 };
 
 exports.student_delete_get = function(req, res, next) {
-    res.send('NOT IMPLEMENTED');
+    res.send('Delete GET not needed at this point');
 };
 
 exports.student_delete_post = function(req, res, next) {
-    res.send('NOT IMPLEMENTED');
+    Student.findByIdAndRemove(req.params.id, function deleteStudent(err) {
+        if (err) {return next(err);}
+        res.status(200).json({'status': 'deleted successfully'});
+    });
 };
 
 exports.student_update_get = function(req, res, next) {
-    res.send('NOT IMPLEMENTED');
+    Student.findById(req.params.id)
+        .exec(function(err, student) {
+            if (err) { return next(err); }
+            if (student == null) {
+                var err = new Error('Student not found');
+                err.status = 404;
+                return next(err);
+            } 
+            res.json(student);
+        });
 };
 
 exports.student_update_post = function(req, res, next) {
-    res.send('NOT IMPLEMENTED');
+    Student.findById(req.params.id, function(err, student) {
+        if (!student) {
+            res.status(404).send("Student not found.");
+        } else {
+            student.name = req.body.name;
+            student.gender = req.body.gender;
+            student.secondary.school = req.body.secondary.school;
+            student.secondary.curriculum = req.body.secondary.curriculum;
+            student.secondary.year = req.body.secondary.year;
+            student.phone_number = req.body.phone_number;
+            student.lessons.subjects = req.body.lessons.subjects;
+            student.lessons.location = req.body.lessons.location;
+
+            student.save()
+                .then(student => {
+                    res.status(200).json({'status': 'updated successfully', 'student': student});
+                })
+                .catch(err => {
+                    res.status(400).send('Updating failed');
+                });
+        }
+    })
 };
 
--- controllers/tutorController.js
+++ controllers/tutorController.js
@@ -3,39 +3,94 @@ var Student = require('../models/student');
 
 var async = require('async');
 
-exports.index = function(req, res) {
+exports.index = function(req, res, next) {
     res.send('NOT IMPLEMENTED');
 };
 
-exports.tutor_list = function(req, res) {
-    res.send('NOT IMPLEMENTED');
+exports.tutor_list = function(req, res, next) {
+    Tutor.find()
+        .exec(function(err, list_tutors) {
+            if (err) {return next(err);}
+            res.json(list_tutors);
+        });
 };
 
 exports.tutor_detail = function(req, res, next) {
-    res.send('NOT IMPLEMENTED');
+    Tutor.findById(req.params.id)
+        .exec(function(err, tutor) {
+            if (err) { return next(err); }
+            if (tutor == null) {
+                var err = new Error('Tutor not found');
+                err.status = 404;
+                return next(err);
+            } 
+            res.json(tutor);
+        });
 };
 
 exports.tutor_create_get = function(req, res, next) {
-    res.send('NOT IMPLEMENTED');
+    res.send('Create GET not needed at this point');
 };
 
 exports.tutor_create_post = function(req, res, next) {
-    res.send('NOT IMPLEMENTED');
+    var tutor = new Tutor(req.body);
+    tutor.save()
+        .then(tutor => {
+            res.status(200).json({'status': 'created successfully', 'tutor': req.body});
+        })
+        .catch(err => {
+            res.status(400).send('creating failed');
+        });
 };
 
 exports.tutor_delete_get = function(req, res, next) {
-    res.send('NOT IMPLEMENTED');
+    res.send('Delete GET not needed at this point');
 };
 
 exports.tutor_delete_post = function(req, res, next) {
-    res.send('NOT IMPLEMENTED');
+    Tutor.findByIdAndRemove(req.params.id, function deleteTutor(err) {
+        if (err) {return next(err);}
+        res.status(200).json({'status': 'deleted successfully'});
+    });
 };
 
 exports.tutor_update_get = function(req, res, next) {
-    res.send('NOT IMPLEMENTED');
+    Tutor.findById(req.params.id)
+        .exec(function(err, tutor) {
+            if (err) { return next(err); }
+            if (tutor == null) {
+                var err = new Error('Tutor not found');
+                err.status = 404;
+                return next(err);
+            } 
+            res.json(tutor);
+        });
 };
 
 exports.tutor_update_post = function(req, res, next) {
-    res.send('NOT IMPLEMENTED');
+    Tutor.findById(req.params.id, function(err, tutor) {
+        if (!tutor) {
+            res.status(404).send("Tutor not found.");
+        } else {
+            tutor.name = req.body.name;
+            tutor.gender = req.body.gender;
+            tutor.university = req.body.university;
+            tutor.secondary.school = req.body.secondary.school;
+            tutor.secondary.curriculum = req.body.secondary.curriculum;
+            tutor.secondary.score = req.body.secondary.score;
+            tutor.phone_number = req.body.phone_number;
+            tutor.lessons.subjects = req.body.lessons.subjects;
+            tutor.lessons.location = req.body.lessons.location;
+            tutor.lessons.time = req.body.lessons.time;
+
+            tutor.save()
+                .then(tutor => {
+                    res.status(200).json({'status': 'updated successfully', 'tutor': tutor});
+                })
+                .catch(err => {
+                    res.status(400).send('Updating failed');
+                });
+        }
+    })
 };
 
