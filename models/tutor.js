156b4f3a0a6e95a51102e4380972e0e7747ff463
Sedrick Keh <sedrickkeh@gmail.com>
2020-03-23 09:39:23 +0800

    minor

--- models/tutor.js
+++ models/tutor.js
@@ -4,6 +4,7 @@ var Schema = mongoose.Schema;
 var TutorSchema = new Schema(
     {
         // tutorImage: {type:String, required:false},
+        title: {type:String, required:true},
         name: {type:String, required:true},
         tutor_id: {type:Number, default:-1},
         pref_name: {type:String},
