a5ee875345d776b16866db76055c1de0a902a11d
Sedrick Keh <sedrickkeh@gmail.com>
2020-05-31 17:50:11 +0800

    cleanup

--- app.js
+++ app.js
@@ -20,10 +20,6 @@ var db = require('./configs/database');
 mongoose.connect(db.config.uri, db.config.options);
 mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
