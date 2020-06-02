6793e04efbd33f9bfbbc17d34bd99b4930175f6e
Sedrick Keh <sedrickkeh@gmail.com>
2020-06-02 19:22:48 +0800

    hash and salt

--- models/users.js
+++ models/users.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
 var Schema = mongoose.Schema;
 
 var UserSchema = new Schema(
@@ -54,6 +55,35 @@ var UserSchema = new Schema(
         timestamps: true
     }
 );
+
+
+UserSchema.pre('save', function(next){
+    var user = this;
+    var SALT_FACTOR = 5;
+
+    if(!user.isModified('password')) {return next();} 
+
+    bcrypt.genSalt(SALT_FACTOR, function(err, salt){
+        if(err) {return next(err);}
+
+        bcrypt.hash(user.password, salt, null, function(err, hash){
+            if(err) {return next(err);}
+            user.password = hash;
+            next();
+        });
+    });
+})
+
+
+UserSchema.methods.comparePassword = function(passwordAttempt, cb){
+    bcrypt.compare(passwordAttempt, this.password, function(err, isMatch) {
+        if(err){
+            return cb(err);
+        } else {
+            cb(null, isMatch);
+        }
+    });
+}
 
 
 module.exports = mongoose.model('User', UserSchema);
