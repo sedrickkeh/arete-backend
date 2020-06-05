6d5da6e6c33a04bcec4000ec56496969279301a0
Sedrick Keh <sedrickkeh@gmail.com>
2020-06-05 17:33:40 +0800

    fix response sender

--- tools/responseSender.js
+++ tools/responseSender.js
@@ -1,15 +1,6 @@
-module.exports.success = function success(data, token=-1, message="ok", status=200) {
-    if (token == null) {
-        return {success: true, 
-            message: message, 
-            status: status, 
-            body: data};
-    }
-    else {
-        return {success: true, 
-            message: message, 
-            status: status, 
-            token: token,
-            body: data};
-    }
+module.exports.success = function success(data, message="ok", status=200) {
+    return {success: true, 
+        message: message, 
+        status: status, 
+        body: data};
 }
